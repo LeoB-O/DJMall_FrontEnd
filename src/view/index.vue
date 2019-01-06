8bd97eb1314d0a997c3f835ad59aa0332ebff189
leob_o <liubobotb@126.com>
2019-01-06 14:26:01 +0800

    init commit

---
+++ src/view/index.vue
@@ -0,0 +1,18 @@
+<template>
+<hello></hello>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+
+export default {
+  name: 'index',
+  components: {
+    'hello': HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
