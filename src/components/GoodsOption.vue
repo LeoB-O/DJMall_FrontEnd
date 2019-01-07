6b5c4d8481d7dfbb5ed96e6d6c6b9a86eddf1a7d
leob_o <liubobotb@126.com>
2019-01-07 10:24:08 +0800

    add: Component GoodsOption

--- src/components/GoodsOption.vue
+++ src/components/GoodsOption.vue
@@ -1,15 +1,47 @@
 <template>
-<div></div>
+<div class="goods-option">
+  <div class="attr-name">{{name}}:</div>
+  <div class="attr-values">
+    <div class="attr-value" v-for="value in values" :key="value">
+      <div>{{value}}</div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 // name string
-// value array
+// values array
 export default {
-  name: 'GoodsOption'
+  name: 'GoodsOption',
+  props: {
+    name: String,
+    values: Array
+  }
 }
 </script>
 
 <style scoped>
+* {
+  display: flex;
+  flex-wrap: wrap;
+}
 
+.goods-option {
+  margin: 10px 0px;
+}
+
+.attr-name {
+  margin: 0px 10px;
+  font-weight: bold;
+  font-size: 24px;
+}
+
+.attr-value {
+  margin: 10px 10px;
+  font-size: 18px;
+  padding: 5px 20px;
+  border: lightgrey solid 1px;
+  border-radius: 5px;
+}
 </style>
--- src/view/GoodsDetail.vue
+++ src/view/GoodsDetail.vue
@@ -1,10 +1,17 @@
 <template>
-<div></div>
+<div>
+  <GoodsOption name="size" :values="[37,38,39,40,41,42,43,44,45,46,47,48,49]"></GoodsOption>
+</div>
 </template>
 
 <script>
+import GoodsOption from '@/components/GoodsOption'
+
 export default {
-  name: 'GoodsDetail'
+  name: 'GoodsDetail',
+  components: {
+    GoodsOption
+  }
 }
 </script>
 
