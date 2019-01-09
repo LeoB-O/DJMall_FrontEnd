03aac4e17a92c9cae77c9ce94f88f79815b518d7
Zeo <zeo@ZeodeMacBook-Pro.local>
2019-01-09 10:11:48 +0800

    add signin and signup

--- src/view/SignIn.vue
+++ src/view/SignIn.vue
@@ -1,13 +1,55 @@
 <template>
-<div></div>
+  <div>
+    <div class="Signin">
+      <div class="Name">
+        <Input v-model="username">
+          <span slot="prepend">UserName</span>
+        </Input>
+      </div>
+
+      <div class="Password">
+        <Input v-model="password" type="password">
+          <span slot="prepend">Password</span>
+        </Input>
+      </div>
+
+      <div class="Button">
+        <Button type="primary">SignIn</Button>
+        <Button type="info" style="margin-left:40px">SignUp</Button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SignIn'
-}
+  name: "SignIn",
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  }
+};
 </script>
 
 <style scoped>
+.Signin {
+  width: 200px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.Name {
+  padding: 5px 5px;
+}
+.Password {
+  padding: 5px 5px;
+}
 
+.Button {
+  float: left;
+  margin-left: 5px
+}
 </style>
--- src/view/SignUp.vue
+++ src/view/SignUp.vue
@@ -1,13 +1,59 @@
 <template>
-<div></div>
+<div>
+  <div class="SignIn">
+    <div class="Input">
+      <span>SignIn</span>
+    </div>
+    <div class="Input">
+      <Input v-model="username">
+        <span slot="prepend">UserName</span>
+      </Input>
+    </div>
+    <div class="Input">
+      <Input v-model="password">
+      <span slot="prepend">Password</span>
+      </Input>
+    </div>
+    <div class="Input">
+      <Input v-model="email">
+      <span slot="prepend">E-mail</span>
+      </Input>
+    </div>
+
+    <div class="Button">
+      <Button type="primary">SignUp</Button>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'SignUp'
+  name: 'SignUp',
+  data(){
+    return{
+      username:'',
+      password:'',
+      email:''
+    }
+  }
 }
 </script>
 
 <style scoped>
+.SignIn{
+  width: 350px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
 
+.Input{
+  padding: 5px 5px;
+}
+
+.Button{
+  margin: 0 auto;
+}
 </style>
